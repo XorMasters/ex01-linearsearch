80b1b1042055e985ed3ed9ab7de2495b90640ab8
XorMasters <aknight@vcccd.edu>
2018-09-10 14:33:07 -0700

    Actually implemented find

--- include/linearsearch.h
+++ include/linearsearch.h
@@ -4,6 +4,11 @@ namespace edu { namespace vcccd { namespace vc { namespace csv15 {
 
 template <class T>
 int64_t find(const T& value, T array[], size_t size) {
+	if (array == NULL) return -1;
+
+	for (int i = 0; i < size; i++) {
+		if (array[i] == value) return i;
+	}
 	return -1;
 }
 
