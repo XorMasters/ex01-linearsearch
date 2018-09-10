ccd7ef3ef99d59131ef4feb231bfc717d12b7c69
XorMasters <aknight@vcccd.edu>
2018-09-10 14:24:55 -0700

    Added using statement

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "linearsearch.h"
 
+using edu::vcccd::vc::csv15::find;
+
 int main(int argc, char *argv[]) {
 	uint64_t values[] = {9, 1, 7, 2, 5, 4, 3, 6, 1, 8};
-	int64_t index = edu::vcccd::vc::csv15::find<uint64_t>(0, values, 9);
+	int64_t index = find<uint64_t>(0, values, 9);
 	std::cout << index << std::endl;
 }
