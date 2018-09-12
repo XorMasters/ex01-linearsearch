8a9e1f4c2319d1b139cb5e757b549f0187f492fc
XorMasters <aknight@vcccd.edu>
2018-09-12 12:45:53 -0700

    Updated main and support lib

--- src/main.cc
+++ src/main.cc
@@ -6,5 +6,5 @@ using edu::vcccd::vc::csv15::find;
 int main(int argc, char *argv[]) {
 	uint64_t values[] = {9, 1, 7, 2, 5, 4, 3, 6, 1, 8};
 	int64_t index = find<uint64_t>(0, values, 9);
-	std::cout << "index: " << index << std::endl;
+	std::cout << "Index: " << index << std::endl;
 }
